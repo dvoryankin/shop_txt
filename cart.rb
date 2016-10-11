5166b8372ed83d27e4196cd738f3f5f0088daef0
pchellovod <pchellovod@gmail.com>
2016-10-12 01:01:29 +0500

    add to cart.rb save_to_file

--- cart.rb
+++ cart.rb
@@ -6,6 +6,21 @@ class Cart
 
   def initialize
     @items = []
+    @owner = owner
+  end
+
+  def save_to_file
+    File.open("#{@owner}_cart.txt", "w") do |f|
+      @items.each { |i| f.puts "#{i.name}:#{i.price}:#{i.weight}"} # car:100:5
+    end
+  end
+
+  def read_from_file
+
   end
 
 end
+
+
+
+
