48086b91e879e44768fd737107a5932afbccde26
pchellovod <pchellovod@gmail.com>
2016-10-12 14:40:44 +0500

    add exception to cart.rb

--- cart.rb
+++ cart.rb
@@ -16,9 +16,14 @@ class Cart
   end
 
   def read_from_file
-    return unless File.exist?("#{@owner}_cart.txt")
+    #return unless File.exist?("#{@owner}_cart.txt")
     File.readlines("#{@owner}_cart.txt").each { |i| @items << i.to_real_item }
     @items.uniq!
+  rescue Errno::ENOENT
+    File.open("#{@owner}_cart.txt", "w") {}
+    puts "File #{@owner}_cart.txt created"
+    end
+
   end
 
 end
