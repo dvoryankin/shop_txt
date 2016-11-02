39ff94a9be41054ae2bdfb9c46bfb3a8f4a98a8b
pchellovod <pchellovod@gmail.com>
2016-11-03 00:56:47 +0500

    change to items << i

--- app/add_to_cart.rb
+++ app/add_to_cart.rb
@@ -3,7 +3,7 @@ require_relative "init"
 cart = Cart.new(ARGV.delete_at(0))
 
 ARGV.each do |a|
-  @items.each { |i| cart.add_item(i) if a == i.name }
+  @items.each { |i| cart.items << i if a == i.name }
 end
 cart.read_from_file
 begin
